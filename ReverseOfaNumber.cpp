0c399c2990c1abaa1916b41a6ac6b3cc38e022f0
Sama Naveen Reddy <samanaveenreddy000@gmail.com>
2024-12-23 19:30:43 +0530

    Reversing of number

---
+++ ReverseOfaNumber.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int number, remainder;
+    string s;
+    do{
+    int  reverseOfaNumber = 0;
+    cout << "Enter any number greater than 10 : ";
+    cin >> number;
+    while(number >= 1){
+        remainder = number % 10;
+        reverseOfaNumber = reverseOfaNumber * 10 + remainder;
+        number = number / 10;
+    }
+    cout << "The Reverse of given number is : " << reverseOfaNumber << endl;
+    cout << "Do you want to Reverse the other number : (yes / no) ";
+    cin >> s;
+    }
+    while(s == "yes");
+    cout << "Thank you for reversig the numbers";
+
+   return 0;
+}
